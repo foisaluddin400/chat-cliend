06953198522dce76c4413af656adebbd1bc30729
foisal <foisalrk2@gmail.com>
2024-10-11 23:25:12 +0600

    fd

--- src/UseAxios.jsx
+++ src/UseAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://chat-box-three-omega.vercel.app'
 })
 
 const UseAxios = () => {
